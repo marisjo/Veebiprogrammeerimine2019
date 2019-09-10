a2e24cb505bd5479244437e486f586183c5f83d9
Maris Jool <marisjo@users.noreply.github.com>
2019-09-10 23:10:34 +0300

    Esimene üleslaadimine git commit -m"esimene üleslaadimine" Esimene.html

---
+++ testpage.php
@@ -0,0 +1,35 @@
+<?php
+  $userName = "Maris Jool";
+?>
+
+<!DOCTYPE html> 
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title >programmeerib veebi</title>
+  <?php 
+    echo $userName;
+  ?>
+  <meta name ="description" content = "See veebileht on loodud katsetamise eesmärgil.">
+  <style>
+body {background-color: #92a8d1;}
+h1   {color: black;}
+p    {color: black;}
+</style>
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName . "veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Teretulemast <em>minu</em> uuele veebilehele!</p>
+  <body bgcolor="white">
+  <body background="sini.jpg" ALT="some text" WIDTH=1920 HEIGHT=1080>
+<main>
+    Siia tulevad minu tehtud pildid
+  </main>
+  <footer>
+   Siia tuleb copyright info ja mõned lingid
+  </footer>
+  </body>
+</html>
