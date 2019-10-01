f756a9c32ee7540bba77906b113216ae1f98b5e7
Maris Jool <marisjo@users.noreply.github.com>
2019-10-01 11:48:01 +0300

    Lisatud andmebaas ning kasutaja andmebaasi

---
+++ Tund05/header.php
@@ -0,0 +1,13 @@
+  
+<?php
+  $pageHeaderHTML = "<!DOCTYPE html> \n";
+  $pageHeaderHTML .= '<html lang="et">'. "\n";
+  $pageHeaderHTML .= "<head> \n";
+  $pageHeaderHTML .= "</head> \n";
+  $pageHeaderHTML .= "<body>";
+  echo $pageHeaderHTML;
+  ?>
+
+ © <?php echo date("Y"); 
+   ?> Copyright.
+
