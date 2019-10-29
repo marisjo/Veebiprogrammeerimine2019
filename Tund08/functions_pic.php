88966294d212717bb96d80183731e207be0f2c2b
Maris Jool <marisjo@users.noreply.github.com>
2019-10-29 11:29:28 +0200

    Tunnitöö lisatud, piltide suurused jne

---
+++ Tund08/functions_pic.php
@@ -0,0 +1,7 @@
+<?php
+
+  function setPicSize($myTempImage, $imageW, $imageH, $imageNewW, $imageNewH){
+	  $myNewImage = imagecreatetruecolor($imageNewW, $imageNewH);
+	  imagecopyresampled($myNewImage, $myTempImage, 0, 0, 0, 0, $imageNewW, $imageNewH, $imageW, $imageH);
+	  return $myNewImage;
+  }
