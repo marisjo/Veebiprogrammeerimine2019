1314627b90ecd464f759ab7c039256e39abd6d4f
Maris Jool <marisjo@users.noreply.github.com>
2019-11-05 11:26:33 +0200

    Tunnitöö, vesimärgi lisamine ning klassid

---
+++ Tund09/classes/Picupload.class.php
@@ -0,0 +1,99 @@
+<?php
+  class Picupload {
+	  private $imageFileType;
+	  private $tmpPic;
+	  private $myTempImage;
+	  private $myNewImage;
+	  
+	  function __construct($tmpPic, $imageFileType){
+		  $this->imageFileType = $imageFileType;
+		  $this->tmpPic = tmpPic;
+		  $this->createImageFromFile();
+		  
+	  }
+	  
+	  function __destruct (){
+		  imagedestroy($this->myTempImage);
+	  }
+	  
+	  private function createImageFromFile() {
+	    if($this->imageFileType  == "jpg" or $this->imageFileType  == "jpeg") {
+				 $this->myTempImage = imagecreatefromjpeg($this->tmpPic);
+		}
+			 
+	    if($this->imageFileType == "png") {
+		    $this->myTempImage = imagecreatefromjpeg($this->tmpPic);
+	    }
+	    if($this->imageFileType == "gif") {
+		    $this->myTempImage = imagecreatefromjpeg($this->tmpPic);
+	    }
+	  }//createImageFromFile lõppeb
+	  
+	  public function resizeImage($maxPicW, $maxPicH){
+		$imageW = imagesx($this->myTempImage);
+	    $imageH = imagesy($this->myTempImage);
+			 
+		if ($imageW > $maxPicW or $imageH > $maxPicH) {
+		if($imageW / $maxPicW > $imageH / $maxPicH) {
+			$picSizeRatio = $imageW / $maxPicW;
+						 
+		}else { 
+		    $picSizeRatio = $imageH / $maxPicH;
+					 
+	    }
+		$imageNewW = round ($imageW / $picSizeRatio, 0);
+		$imageNewH = round ($imageH / $picSizeRatio, 0);
+	    $this->myNewImage = $this->setPicSize($this->myTempImage, $imageW, $imageH, $imageNewW, $imageNewH);
+	  }
+	 }//resizeImage lõppeb
+	 
+	 public function addWatermark($wmFile) {
+		 $waterMark = imagecreatefrompng($wmFile);
+		 $waterMarkW = imagesx($waterMark);
+		 $waterMarkH = imagesy($waterMark);
+		 $waterMarkX = imagesx($this->myNewImage) - $waterMarkW - 10;
+		 $waterMarkY = imagesy($this->myNewImage) - $waterMarkH - 10;
+		 imagecopy($this->myNewImage, $waterMarkW, $waterMarkX, $waterMarkY, 0, 0, $waterMarkW, $waterMarkH);
+	 }//addWatermark lõppeb
+	 
+	  private function setPicSize($myTempImage, $imageW, $imageH, $imageNewW, $imageNewH){
+	    $myNewImage = imagecreatetruecolor($imageNewW, $imageNewH);
+	    imagecopyresampled($myNewImage, $myTempImage, 0, 0, 0, 0, $imageNewW, $imageNewH, $imageW, $imageH);
+	    return $myNewImage;
+	  
+	  }//setPicSize lõppeb 
+	  
+	  public function savePicFile($filename){
+		  
+		if($this->imageFileType == "jpg" or $this->imageFileType == "jpeg"){
+			if(imagejpeg($this->myNewImage, $filename, 90)){
+			    $notice = "Vähendatud faili salvestamine õnnestus!";
+		    } else {
+			    $notice = "Vähendatud faili salvestamine ei õnnestunud!";
+			}
+					
+		}
+	    if($this->imageFileType == "png"){
+			if(imagepng($this->myNewImage, $filename, 6)){
+				$notice = "Vähendatud faili salvestamine õnnestus!";
+			} else {
+				$notice = "Vähendatud faili salvestamine ei õnnestunud!";
+			}
+					
+		}
+		if($this->imageFileType == "gif"){
+			if(imagegif($this->myNewImage, $filename)){
+				$notice = "Vähendatud faili salvestamine õnnestus!";
+			} else {
+				$notice = "Vähendatud faili salvestamine ei õnnestunud!";
+			}
+					
+		}
+		
+		imagedestroy($myNewImage);	
+		imagedestroy($myTempImage);
+		return $notice;
+	  }//savePicFile lõppeb
+	  
+	  
+  }//class lõppeb
---
+++ Tund09/picupload.php
@@ -0,0 +1,142 @@
+<?php
+  require("../../../config_vp2019.php");
+  require("functions_user.php");
+  require("functions_main.php");
+  require("functions_pic.php");
+  //require("classes/Test.class.php");
+  require("classes/Picupload.class.php");
+  $database = "if19_maris_jo_1";
+  
+  $userName = $_SESSION["userFirstname"] ." " .$_SESSION["userLastname"];
+
+ 
+  
+    //kui pole sisseloginud
+  if(!isset($_SESSION["userID"])){
+	  //siis jõuga sisselogimise lehele
+	  header("Location: page.php");
+	  exit();
+  }
+  
+  //väljalogimine
+  if(isset($_GET["logout"])){
+	  session_destroy();
+	  header("Location: page.php");
+	  exit();
+  }
+  
+  //$myTest = new Test(20);
+  //echo $myTest->privateNumber;
+  //echo $myTest->tellPublicSecret();
+  //unset($myTest);
+  //echo $myTest->tellPublicSecret();
+  
+  $userName = $_SESSION["userFirstname"] ." " .$_SESSION["userLastname"];
+  
+  $notice = null;
+  $maxPicW = 600;
+  $maxPicH = 400;
+  
+  //var_dump($_POST);
+  //var_dump($_FILES);
+  
+  //pildi üleslaadimise osa
+	//$target_dir = "uploads/";
+	//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	
+	$uploadOk = 1;
+	
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submitPic"])) {
+		//$target_file = $pic_upload_dir_orig . basename($_FILES["fileToUpload"]["name"]);
+		//$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		$imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION));
+		$filename = "vp_";
+		$timeStamp = microtime(1) * 10000;
+		$filename .= $timeStamp ."." .$imageFileType;
+		$target_file = $pic_upload_dir_orig .$filename;
+		
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}
+	
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 2500000) {
+			echo "Sorry, your file is too large.";
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+		} else {
+			
+			//kasutame klassi
+			$myPic = new Picupload($_FILES["fileToUpload"]["tmp_name"], $imageFileType);
+			//teeme pildi väiksemaks
+			$myPic->resizeImage($maxPicW, $MmaxPicH);
+			//lisame vesimärgi
+			$myPic->addWatermark("../vp_pics/vp_logo_w100_overlay.png");
+			//loeme pildifaili sisu pikslikogumiks ehk "pildiobjektiks"
+			//kirjutame vähendatud pildi faili
+			$myPic->savePicFile($pic_upload_dir_w600 .$filename);
+			unset($myPic);
+						
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
+			
+			//salvestan info andmebaasi
+			$notice .= addPicData($filename, test_input($_POST["altText"]), $_POST["privacy"]);
+		}
+	
+	}//kas nuppu klikiti
+  
+  //_____________________________________________
+  
+  require("header.php");
+?>
+
+  <?php
+    echo "<h1>" .$userName ." koolitöö leht</h1>";
+  ?>
+  <p>See leht on loodud koolis õppetöö raames
+  ja ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p><a href="?logout=1">Logi välja!</a> | Tagasi <a href="home.php">avalehele</a></p>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
+	  <label>Vali üleslaetav pildifail!</label><br>
+	  <input type="file" name="fileToUpload" id="fileToUpload">
+	  <br>
+	  <label>Alt tekst: </label><input type="text" name="altText">
+	  <br>
+	  <label>Privaatsus</label>
+	  <br>
+	  <input type="radio" name="privacy" value="1"><label>Avalik</label>&nbsp;
+	  <input type="radio" name="privacy" value="2"><label>Sisseloginud kasutajatele</label>&nbsp;
+	  <input type="radio" name="privacy" value="3" checked><label>Isiklik</label>
+      <br>
+	  <input name="submitPic" type="submit" value="Lae pilt üles!"><span><?php echo $notice; ?></span>
+	</form>
+	<hr>
+	
+</body>
+</html>
